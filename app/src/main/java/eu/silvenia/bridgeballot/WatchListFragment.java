e01a46b9cb4f24f9bb47ff4fd1d5d6002c4f6c2e
gamerszvidx <gamerszvidx@hotmail.com>
2015-06-05 14:08:15 +0200

    added a dynamically changing Listview layout in the watchlist page (static data)

--- app/src/main/java/eu/silvenia/bridgeballot/WatchListFragment.java
+++ app/src/main/java/eu/silvenia/bridgeballot/WatchListFragment.java
@@ -8,49 +8,62 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ActionMenuView;
+import android.widget.AdapterView;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class WatchListFragment extends Fragment {
+    ListView list;
+    String[] itemname = {
+            "Safari",
+            "Camera",
+            "Global",
+            "FireFox",
+            "UC Browser",
+            "Android Folder",
+            "VLC Player",
+            "Cold War"
+    };
+
+    Integer[] imgid = {
+            R.mipmap.ic_redcircle,
+            R.mipmap.ic_redcircle,
+            R.mipmap.ic_redcircle,
+            R.mipmap.ic_redcircle,
+            R.mipmap.ic_redcircle,
+            R.mipmap.ic_redcircle,
+            R.mipmap.ic_redcircle,
+            R.mipmap.ic_redcircle,
+    };
     View rootview;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootview = inflater.inflate(R.layout.fragment_watch_list, container, false);
 
-        RelativeLayout topTitle = new RelativeLayout(getActivity());
+        WatchListAdapter adapter = new WatchListAdapter(getActivity(), itemname, imgid);
+        list = (ListView) rootview.findViewById(R.id.list);
+        list.setAdapter(adapter);
 
-        RelativeLayout.LayoutParams topTitleParam = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,RelativeLayout.LayoutParams.MATCH_PARENT);
-        topTitle.setPadding(0, 15, 0, 0);
-        topTitle.setLayoutParams(topTitleParam);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-        ViewGroup.LayoutParams fragmentTitleParam = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        TextView fragmentTitle = new TextView(getActivity());
-        fragmentTitle.setGravity(Gravity.CENTER);
-        fragmentTitle.setText("Watchlist");
-        fragmentTitle.setTextSize(40.0f);
-        fragmentTitle.setId(10);
-        topTitle.addView(fragmentTitle, fragmentTitleParam);
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                String Slecteditem = itemname[+position];
+                Toast.makeText(getActivity(), Slecteditem, Toast.LENGTH_SHORT).show();
 
-        RelativeLayout.LayoutParams bridgeLayoutParam = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, 300);
+            }
+        });
 
-        RelativeLayout bridgeLayout = new RelativeLayout(topTitle.getContext());
-        bridgeLayout.setId(11);
-        TextView fragmentTitle2 = new TextView(bridgeLayout.getContext());
-        fragmentTitle2.setGravity(Gravity.CENTER);
-        fragmentTitle2.setText("test");
-        fragmentTitle2.setTextSize(25.0f);
-        bridgeLayout.addView(fragmentTitle2, fragmentTitleParam);
-
-        bridgeLayoutParam.addRule(RelativeLayout.BELOW, fragmentTitle.getId());
-        bridgeLayoutParam.setMargins(0, 20, 0, 20);
-
-        topTitle.addView(bridgeLayout, bridgeLayoutParam);
-
-        return topTitle;
+        return rootview;
     }
 }
