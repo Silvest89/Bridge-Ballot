15a1ce16ca29acbf0d6777015894943a9ec92497
jvanbreda <jvbreda1995@gmail.com>
2015-06-09 15:22:11 +0200

    Some bug fixes

--- app/src/main/java/eu/silvenia/bridgeballot/MainActivity.java
+++ app/src/main/java/eu/silvenia/bridgeballot/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends Activity implements
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startService(new Intent(this, GPSservice.class));
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
